95c5482a271f190a9109a77bba27cf0426e4d9eb
Daniel Valenzuela <danielpipe2203@gmail.com>
2024-03-01 21:19:27 -0500

    menu component v1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MenuNav />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MenuNav from './components/MenuNav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MenuNav
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+</style>./components/MenuNav.vue
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        electricYellow: '#F5F749',
+        softBlue: '#2E86AB',
+        mainBlack: '#0D160B',
+        softGray: '#DBDBDF',
+        alertRed: '#720E07'
+      },
+      fontFamily: {
+        visueltProBld: ["Visuelt ProBold", "sans-serif"],
+        visueltPro: ["Visuelt ProRegular", "sans-serif"],
+        visueltLg: ["Visuelt ProLight", "sans-serif"],
+        Inter: ["Inter", "sans-serif"]
+      }
+    }
   },
   plugins: [],
 }
