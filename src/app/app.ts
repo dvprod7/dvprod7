051ccf05bca9c70786402de6f7e8e439851ec938
Daniel Felipe Valenzuela <danielpipe2203@gmail.com>
2026-01-09 18:06:34 -0500

    fullscreen scroll snap layout with fixed navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { Navbar } from "./components/navbar/navbar";
+import { Hero } from "./components/hero/hero";
 /* import { RouterOutlet } from '@angular/router'; */
 
 @Component({
@@ -7,7 +8,7 @@ import { Navbar } from "./components/navbar/navbar";
   /* imports: [RouterOutlet], */
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [Navbar]
+  imports: [Navbar, Hero]
 })
 export class App {
   protected readonly title = signal('dvprod7-app');
