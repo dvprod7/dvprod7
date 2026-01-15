a92e98b582c61359d9eab9da12188f3fddf479ca
Daniel Felipe Valenzuela <danielpipe2203@gmail.com>
2026-01-14 19:27:01 -0500

    about section start

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
 /* import { RouterOutlet } from '@angular/router'; */
 
 @Component({
@@ -8,7 +9,7 @@ import { Hero } from './components/hero/hero';
   /* imports: [RouterOutlet], */
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [Navbar, Hero],
+  imports: [Navbar, Hero, About],
 })
 export class App {
   protected readonly title = signal('dvprod7-app');
