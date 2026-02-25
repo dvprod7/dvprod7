c1d393b82a409ed3a154d6f6c0e6a3308220ec0a
Daniel Felipe Valenzuela <danielpipe2203@gmail.com>
2026-02-25 14:57:21 -0500

    progress to date

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
 import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
 /* import { RouterOutlet } from '@angular/router'; */
 
 @Component({
@@ -9,7 +10,7 @@ import { About } from './components/about/about';
   /* imports: [RouterOutlet], */
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [Navbar, Hero, About],
+  imports: [Navbar, Hero, About, Skills],
 })
 export class App {
   protected readonly title = signal('dvprod7-app');
